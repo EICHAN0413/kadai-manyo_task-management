b59ecb3148d1c6d3a921a5fdeb3afd89f544964f
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-08-17 11:58:26 +0900

    Rspecの準備完了

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,7 +2,7 @@ module TasksHelper
     def choose_new_or_edit
         if action_name == 'new' || action_name == 'create'
             confirm_tasks_path
-        elsif action == 'edit'
+        elsif action_name == 'edit'
             task_path
         end
     end
