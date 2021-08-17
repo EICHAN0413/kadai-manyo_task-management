5906409d985609f52a4878ec69288611ddd60fba
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-08-17 10:29:38 +0900

    タスクの新規、詳細、編集機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,36 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit ]
+ 
   def index
     @tasks = Task.all
   end
 
   def show
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスクを追加しました"
+    else
+      render :new
+    end
+  end  
+
+  def edit
+  end
+
+  private
+  def set_task
+    @task =Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
