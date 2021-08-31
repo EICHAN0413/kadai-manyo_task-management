178e9443387d13471d76057b2c968749fc58bc74
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-09-01 00:05:53 +0900

    seedデータを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-User.create(
+User.create!(
     name: '池田英輝', 
     email: 'chanike1993@yahoo.co.jp',
-    password_digest: '22493629'
+    password: '22493629',
+    password_confirmation: '22493629',
+    admin: false
 )
 
 User.create!(
-    name:  "管理者",admin@example.jp
-    email: "",
+    name:  "管理者",
+    email: "admin@example.jp",
     password:  "11111111",
     password_confirmation: "11111111",
-    admin: true)
+    admin: true
+)
